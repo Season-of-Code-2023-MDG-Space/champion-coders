07a61a6608dfe095a7c393ae4e4e6928e5dfafc4
Ann18532 <122373286+Ann18532@users.noreply.github.com>
2023-03-11 19:23:24 +0530

    Update api.py

--- bot-codes/api.py
+++ bot-codes/api.py
@@ -1,14 +1,16 @@
 import requests     
 import json
+import random
 
 class apiView():
         def apiCall():
             response_API = requests.get('https://the-trivia-api.com/api/questions?limit=1')
             data = response_API.text
             #print(response_API.status_code)
-            print(data)
+            #print(data)
             q_list = json.loads(data)
             z=q_list[0]
+            k=0
             # corrans=data[0]
             # listAll=data[0][3]
                     #Testing for api
@@ -17,7 +19,12 @@ class apiView():
             ques=z["question"]
             rightans = z["correctAnswer"]
             b.append(a)
-            dictTest=[a,b,ques]
+            random.shuffle(b)
+            for i in range(0,4):
+                  if b[i]==a:
+                        k=i
+                        break
+
+            dictTest=[a,b,ques,k]
             return dictTest
             
-# listAll.append(corrans)
